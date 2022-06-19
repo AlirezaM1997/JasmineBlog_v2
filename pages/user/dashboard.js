779709cde549b06aa218c7c4935887ea70ac83ef
Alireza <armahmodi7@gmail.com>
2022-06-19 15:17:16 +0430

    change username test commit

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,46 +1,43 @@
-
-// import { useAllState } from "../../context/state";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { getCookie } from "cookies-next";
+import Loading from "../../components/loading";
 
-const Dashboards=()=> {
-  return <h2>hiiiiiiiiiiiiiiiiiiiiiii</h2>;
-}
-
+const Dashboard = () => {
+  return <h2>This is Dashboard......................</h2>;
+};
 const withAuth = (Component) => {
-
   const AuthenticatedComponent = () => {
     const router = useRouter();
-    const [data, setData] = useState()
-    
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
       const getUser = async () => {
-            // const { token } = useAllState();
-            const response = await fetch("http://localhost:4000/user/me", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-                auth: `ut ${getCookie('token')}`,
-              },
-              body: JSON.stringify({}),
-            });
-              const userData = await response.json();
-              console.log('%c userData','background:blue',userData);
-              if (userData && userData._id) {
-                setData(userData);
-              } else {
-                router.push('/validation/login');
-              }  
-          };
-          getUser();
-      }, []);
-
-      return data ? <Component /> : null; 
+        const response = await fetch("http://localhost:4000/user/me", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            auth: `ut ${getCookie("token")}`,
+          },
+          body: JSON.stringify({}),
+        });
+        const userData = await response.json();
+        console.log("%c userData", "background:blue", userData);
+        if (userData && userData._id) {
+          setData(userData);
+        } else {
+          router.push("/validation/login");
+        }
+        setLoading(false)
+      };
+      getUser();
+    }, []);
+    if (loading) {
+      return <Loading />;
+    } else {
+      return data ? <Component /> : null;
+    }
   };
-
   return AuthenticatedComponent;
 };
-
-
-export default withAuth(Dashboards);
+export default withAuth(Dashboard);
