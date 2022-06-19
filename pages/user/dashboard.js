5c4dc7bf5f09e1a1dcd50447bf99628a2befd535
Alireza <armahmodi7@gmail.com>
2022-06-19 13:56:23 +0430

    auth done! commit

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,13 +1,46 @@
-//components
-// import Dashboard from "../../components/dashboard";
-import RequireAuth from "../../components/requireAuth";
 
-function dashboard() {
-  return (
-    <RequireAuth>
-     <h2>hiiiiiiiiiiiiiiiiiiiiiii</h2>
-    </RequireAuth>
-  );
+// import { useAllState } from "../../context/state";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { getCookie } from "cookies-next";
+
+const Dashboards=()=> {
+  return <h2>hiiiiiiiiiiiiiiiiiiiiiii</h2>;
 }
 
-export default dashboard;
+const withAuth = (Component) => {
+
+  const AuthenticatedComponent = () => {
+    const router = useRouter();
+    const [data, setData] = useState()
+    
+    useEffect(() => {
+      const getUser = async () => {
+            // const { token } = useAllState();
+            const response = await fetch("http://localhost:4000/user/me", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                auth: `ut ${getCookie('token')}`,
+              },
+              body: JSON.stringify({}),
+            });
+              const userData = await response.json();
+              console.log('%c userData','background:blue',userData);
+              if (userData && userData._id) {
+                setData(userData);
+              } else {
+                router.push('/validation/login');
+              }  
+          };
+          getUser();
+      }, []);
+
+      return data ? <Component /> : null; 
+  };
+
+  return AuthenticatedComponent;
+};
+
+
+export default withAuth(Dashboards);
